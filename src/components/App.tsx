caeabb89b5ed1755029379d1f0e66e9a64c132a3
RazorzMcLazorz <nateyoungdev@gmail.com>
2020-12-20 17:29:10 -0700

    added puppeteer but cant use yet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react'
+// import puppeteer from 'puppeteer'
 
 interface IProps {}
 
 interface IState {}
 
 class App extends React.Component<IProps, IState> {
+  async grabExample() {
+    // const browser = await puppeteer.launch()
+    //   const page = await browser.newPage()
+    //   await page.goto('https://example.com')
+
+    //   // await browser.close()
+  }
+
+  componentDidMount() {}
+
   render() {
-    return <div>
-      enter phone number
-    </div>
+    return <div>enter phone number</div>
   }
 }
 
